e5390062dfcaa9acc418c6d02834531448315a74
Anshu Waghmare <162833925+anshuw1@users.noreply.github.com>
2024-07-22 15:27:51 +0530

    Update grafana.sh

--- grafana.sh
+++ grafana.sh
@@ -1,7 +1,25 @@
+# Install necessary dependencies
+sudo apt-get install -y adduser libfontconfig1
 
+GRAF_LATEST_VERSION= 
 
-GRAF_V_LATEST_VERSION=v11.1.0 
-GRAF_LATEST_VERSION=v11.1.0 
+# Construct the download URL for the latest version
+DOWNLOAD_URL="https://dl.grafana.com/enterprise/release/grafana-enterprise_${LATEST_VERSION#v}_amd64.deb"
 
+# Download the latest version of Grafana Enterprise
+wget $DOWNLOAD_URL -O grafana-enterprise_latest_amd64.deb
 
+# Install Grafana Enterprise
+sudo dpkg -i grafana-enterprise_latest_amd64.deb
+
+# Reload systemd, enable, and start Grafana service
+sudo /bin/systemctl daemon-reload
+sudo /bin/systemctl enable grafana-server
+sudo /bin/systemctl start grafana-server
+
+# Check Grafana service status
+sudo /bin/systemctl status grafana-server --no-pager
+
+# Clean up downloaded .deb file
+rm grafana-enterprise_latest_amd64.deb
 .
