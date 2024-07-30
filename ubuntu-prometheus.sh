c0f4203cbf69e14ae56984dffcdb524bfe3d10bf
Anshu Waghmare <162833925+anshuw1@users.noreply.github.com>
2024-07-30 15:09:02 +0530

    Update ubuntu-prometheus.sh

--- ubuntu-prometheus.sh
+++ ubuntu-prometheus.sh
@@ -9,3 +9,24 @@ if ! command -v wget &> /dev/null; then
     sudo apt-get update
     sudo apt-get install -y wget
 fi 
+
+# Construct the download URL
+DOWNLOAD_URL="https://github.com/prometheus/prometheus/releases/download/v${LATEST_VERSION}/prometheus-${LATEST_VERSION}.linux-amd64.tar.gz"
+
+# Download the latest version of Prometheus
+wget $DOWNLOAD_URL
+
+# Extract the downloaded tarball
+tar -xf prometheus-${LATEST_VERSION}.linux-amd64.tar.gz
+
+# Move the binaries to /usr/local/bin
+sudo mv prometheus-${LATEST_VERSION}.linux-amd64/prometheus prometheus-${LATEST_VERSION}.linux-amd64/promtool /usr/local/bin
+
+# Create directories for configuration files and other Prometheus data
+sudo mkdir -p /etc/prometheus /var/lib/prometheus
+
+# Move console libraries
+sudo mv prometheus-${LATEST_VERSION}.linux-amd64/console_libraries /etc/prometheus
+
+# Clean up the extracted files
+sudo rm -rvf prometheus-${LATEST_VERSION}.linux-amd64*
