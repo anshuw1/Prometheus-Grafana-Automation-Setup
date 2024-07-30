4aab24b33ab59fe9e27d191343d6e49eda4adba4
Anshu Waghmare <162833925+anshuw1@users.noreply.github.com>
2024-07-30 15:04:13 +0530

    Update ubuntu-grafana.sh

--- ubuntu-grafana.sh
+++ ubuntu-grafana.sh
@@ -3,3 +3,12 @@ sudo apt-get update
 
 # Fetch the latest Grafana version
 LATEST_VERSION=11.1.0 
+
+# Install necessary dependencies
+sudo apt-get install -y adduser libfontconfig1
+
+# Construct the download URL for the latest version
+DOWNLOAD_URL="https://dl.grafana.com/enterprise/release/grafana-enterprise_${LATEST_VERSION}_amd64.deb"
+
+# Download the latest version of Grafana Enterprise
+wget $DOWNLOAD_URL -O grafana-enterprise_latest_amd64.deb
