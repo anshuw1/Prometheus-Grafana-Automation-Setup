c5819eb3e5d731f3f582ffcf87ff344fb0444e16
Anshu Waghmare <162833925+anshuw1@users.noreply.github.com>
2024-07-30 15:08:18 +0530

    Update ubuntu-prometheus.sh

--- ubuntu-prometheus.sh
+++ ubuntu-prometheus.sh
@@ -2,3 +2,10 @@
 
 # Fetched Latest Prometheus Version
 LATEST_VERSION=2.53.1
+
+# Check if wget is installed, if not, install it
+if ! command -v wget &> /dev/null; then
+    echo "wget could not be found, installing it..."
+    sudo apt-get update
+    sudo apt-get install -y wget
+fi 
