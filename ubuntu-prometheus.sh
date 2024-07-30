628788ff2d9bf5a45816dd26aa9936dbaf89ac18
Anshu Waghmare <162833925+anshuw1@users.noreply.github.com>
2024-07-30 15:09:49 +0530

    Update ubuntu-prometheus.sh

--- ubuntu-prometheus.sh
+++ ubuntu-prometheus.sh
@@ -30,3 +30,53 @@ sudo mv prometheus-${LATEST_VERSION}.linux-amd64/console_libraries /etc/promethe
 
 # Clean up the extracted files
 sudo rm -rvf prometheus-${LATEST_VERSION}.linux-amd64*
+
+# Create Prometheus configuration file
+sudo tee /etc/prometheus/prometheus.yml > /dev/null <<EOF
+global:
+  scrape_interval: 10s
+
+scrape_configs:
+  - job_name: 'prometheus_metrics'
+    scrape_interval: 5s
+    static_configs:
+      - targets: ['localhost:9090']
+  - job_name: 'node_exporter_metrics'
+    scrape_interval: 5s
+    static_configs:
+      - targets: ['localhost:9100','worker-1:9100','worker-2:9100']
+EOF
+
+# Create Prometheus user
+sudo useradd -rs /bin/false prometheus
+
+# Change ownership of the directories
+sudo chown -R prometheus: /etc/prometheus /var/lib/prometheus
+
+# Create systemd service file for Prometheus
+sudo tee /etc/systemd/system/prometheus.service > /dev/null <<EOF
+[Unit]
+Description=Prometheus
+After=network.target
+
+[Service]
+User=prometheus
+Group=prometheus
+Type=simple
+ExecStart=/usr/local/bin/prometheus \
+    --config.file /etc/prometheus/prometheus.yml \
+    --storage.tsdb.path /var/lib/prometheus/ \
+    --web.console.templates=/etc/prometheus/consoles \
+    --web.console.libraries=/etc/prometheus/console_libraries
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Reload systemd and enable Prometheus service
+sudo systemctl daemon-reload
+sudo systemctl enable prometheus
+
+# Start Prometheus service and check status
+sudo systemctl start prometheus
+sudo systemctl status prometheus --no-pager
